2643cc495e15392c1f105fdd03d581a59d2c1808
Modex <Spatz343@gmail.com>
2020-03-12 22:33:36 +0100

    Bugfixes

--- ArcherionBungee/src/de/Modex/bungee/commands/reply.java
+++ ArcherionBungee/src/de/Modex/bungee/commands/reply.java
@@ -3,14 +3,15 @@ package de.Modex.bungee.commands;
 import de.Modex.bungee.utils.Data;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
-public class reply extends Command {
+public class reply extends Command{
 
     public reply() {
-        super("reply", "bungee.whisper", "r");
+        super("r", "bungee.whisper", "reply");
     }
 
     @Override
@@ -21,17 +22,18 @@ public class reply extends Command {
         }
         ProxiedPlayer p = (ProxiedPlayer) sender;
 
-        if (args.length > 1) {
+        if (args.length != 0) {
             if (Data.replyList.containsKey(p)) {
                 ProxiedPlayer target = Data.replyList.get(p);
                 StringBuilder msg = new StringBuilder();
 
-                for (int i = 1; i < args.length; i++) {
+                for (int i = 0; i < args.length; i++) {
                     msg.append(args[i]).append(" ");
                 }
 
-                p.sendMessage(ChatMessageType.CHAT, new TextComponent("§EFROM " + target.getName() + " §8» §a" + msg));
-                target.sendMessage(ChatMessageType.CHAT, new TextComponent("§aTO " + target.getName() + " §8» §a" + msg));
+                String done = msg.toString().replace("&", "§");
+                target.sendMessage(ChatMessageType.CHAT, new TextComponent("§eFROM §7" + p.getName() + " §8» §3" + done));
+                p.sendMessage(ChatMessageType.CHAT, new TextComponent("§aTO §7" + target.getName() + " §8» §3" + done));
             } else {
                 p.sendMessage(ChatMessageType.CHAT, new TextComponent(Data.prefix + "§cYou did not whisper recently!"));
             }
--- ArcherionBungee/src/de/Modex/bungee/commands/whisper.java
+++ ArcherionBungee/src/de/Modex/bungee/commands/whisper.java
@@ -7,11 +7,15 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.TabExecutor;
 
-public class whisper extends Command {
+import java.util.HashSet;
+import java.util.Set;
+
+public class whisper extends Command implements TabExecutor {
 
     public whisper() {
-        super("whisper", "bungee.whisper", "w", "msg", "dm");
+        super("w", "bungee.whisper", "whisper", "msg", "dm");
     }
 
     @Override
@@ -22,7 +26,7 @@ public class whisper extends Command {
         }
         ProxiedPlayer p = (ProxiedPlayer) sender;
 
-        if (args.length > 2) {
+        if (args.length > 1) {
             ProxiedPlayer target = ProxyServer.getInstance().getPlayer(args[0]);
 
             if (target != null) {
@@ -33,8 +37,10 @@ public class whisper extends Command {
                         msg.append(args[i]).append(" ");
                     }
 
-                    p.sendMessage(ChatMessageType.CHAT, new TextComponent("§EFROM " + target.getName() + " §8» §a" + msg));
-                    target.sendMessage(ChatMessageType.CHAT, new TextComponent("§aTO " + target.getName() + " §8» §a" + msg));
+                    String done = msg.toString().replace("&", "§");
+
+                    target.sendMessage(ChatMessageType.CHAT, new TextComponent("§eFROM §7" + p.getName() + " §8» §3" + done));
+                    p.sendMessage(ChatMessageType.CHAT, new TextComponent("§aTO §7" + target.getName() + " §8» §3" + done));
 
                     Data.replyList.put(p, target);
                     Data.replyList.put(target, p);
@@ -47,5 +53,19 @@ public class whisper extends Command {
         } else {
             p.sendMessage(ChatMessageType.CHAT, new TextComponent(Data.prefix + "§c/whisper <Player> <Message>"));
         }
+    }
+
+    @Override
+    public Iterable<String> onTabComplete(CommandSender sender, String[] args) {
+        Set<String> matches = new HashSet<>();
+        if (args.length == 1) {
+            String search = args[0].toLowerCase();
+            for (ProxiedPlayer player : ProxyServer.getInstance().getPlayers()) {
+                if (player.getName().toLowerCase().startsWith(search)) {
+                    matches.add(player.getName());
+                }
+            }
+        }
+        return matches;
     }
 }
